ceba09669a3e87f70971fc722943b17b370f7e48
CloneFrostt <141675516+CloneFrostt@users.noreply.github.com>
2023-10-03 17:13:53 -0300

    Lo que terminamos hoy

--- src/Data/LibroData.java
+++ src/Data/LibroData.java
@@ -5,40 +5,45 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class LibroData {
-   private Connection con = null;
-   
+
+    private Connection con = null;
+
     public LibroData() {
 
         con = Conexion.getConexion();
     }
-    public void cargarLibro(Libros libro){
-    String sql= "INSERT INTO libro(isbn,Titulo,Autor,Anio,Tipo,Editorial,Estado)VALUES (?,?,?,?,?,?,?)";
-       try {
-           PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-           ps.setString(1,libro.getIsbn());
-           ps.setString(2,libro.getTitulo());
-           ps.setString(3,libro.getAutor());
-           ps.setInt(4,libro.getAnio());
-           ps.setString(5,libro.getTipo());
-           ps.setString(6,libro.getEditorial());
-           ps.setBoolean(7,libro.isEstado());
-           ps.executeUpdate();
-           ResultSet rs=ps.getGeneratedKeys();
-           if(rs.next()){
-           libro.setIdLibro(rs.getInt(1));
-              JOptionPane.showMessageDialog(null, "Libro añadido con exito.");
-              
-           } 
-           ps.close();
-       } catch (SQLException ex) {
-             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Libro");
-       }
+
+    public void cargarLibro(Libros libro) {
+        String sql = "INSERT INTO libro(isbn,Titulo,Autor,Anio,Tipo,Editorial,Estado)VALUES (?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, libro.getIsbn());
+            ps.setString(2, libro.getTitulo());
+            ps.setString(3, libro.getAutor());
+            ps.setInt(4, libro.getAnio());
+            ps.setString(5, libro.getTipo());
+            ps.setString(6, libro.getEditorial());
+            ps.setBoolean(7, libro.isEstado());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                libro.setIdLibro(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Libro añadido con exito.");
+
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Libro");
+        }
     }
-    private void eliminarLibro(int idLibro){
-     try {
+
+    public void eliminarLibro(int idLibro) {
+        try {
             String sql = "UPDATE libro SET estado = 0 WHERE idLibro = ? ";
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, idLibro);
@@ -52,4 +57,124 @@ public class LibroData {
             JOptionPane.showMessageDialog(null, " Error al acceder a la tabla Libro");
         }
     }
+
+    public void modificarLibro(Libros libro) {
+        String sql = "UPDATE libro SET  titulo=?, autor=?,tipo=?,anio=?,editorial=?"
+                + "WHERE idLibro=?";
+        PreparedStatement ps = null;
+
+        try {
+            ps = con.prepareStatement(sql);
+
+            ps.setString(1, libro.getTitulo());
+            ps.setString(2, libro.getAutor());
+            ps.setString(3, libro.getTipo());
+            ps.setInt(4, libro.getAnio());
+            ps.setString(5, libro.getEditorial());
+            ps.setInt(6, libro.getIdLibro());
+            int exito = ps.executeUpdate();
+
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Modificado Exitosamente.");
+            } else {
+                JOptionPane.showMessageDialog(null, "El Libro ya existe");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Libros " + ex.getMessage());
+        }
+    }
+
+    public List<Libros> listarLibros() {
+
+        List<Libros> libro = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM libro WHERE estado = 1";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Libros lib = new Libros();
+
+                lib.setIdLibro(rs.getInt("idLibro"));
+                lib.setIsbn(rs.getString("isbn"));
+                lib.setTitulo(rs.getString("Titulo"));
+                lib.setAutor(rs.getString("Autor"));
+                lib.setAnio(rs.getInt("Anio"));
+                lib.setTipo(rs.getString("Tipo"));
+                lib.setEditorial(rs.getString("Editorial"));
+                lib.setEstado(rs.getBoolean("Estado"));
+                libro.add(lib);
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, " Error al acceder a la tabla Libros" + ex.getMessage());
+        }
+        return libro;
+    }
+
+    public Libros buscarLibroPorAutor(String autor) {
+        Libros lib = null;
+        String sql = "SELECT * FROM libro WHERE Autor=?";
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, autor);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                lib = new Libros();
+                
+                lib.setIdLibro(rs.getInt("idLibro"));
+                lib.setIsbn(rs.getString("isbn"));
+                lib.setTitulo(rs.getString("Titulo"));
+                lib.setAutor(rs.getString("Autor"));
+                lib.setAnio(rs.getInt("Anio"));
+                lib.setTipo(rs.getString("Tipo"));
+                lib.setEditorial(rs.getString("Editorial"));
+                lib.setEstado(rs.getBoolean("Estado"));
+
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe el Libro");
+
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Libros " + ex.getMessage());
+        }
+
+        return lib;
+    }
+public Libros buscarLibroPorId(String idLibro) {
+        Libros lib = null;
+        String sql = "SELECT * FROM libro WHERE idLibro=?";
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, idLibro);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                lib = new Libros();
+                
+                lib.setIdLibro(rs.getInt("idLibro"));
+                lib.setIsbn(rs.getString("isbn"));
+                lib.setTitulo(rs.getString("Titulo"));
+                lib.setAutor(rs.getString("Autor"));
+                lib.setAnio(rs.getInt("Anio"));
+                lib.setTipo(rs.getString("Tipo"));
+                lib.setEditorial(rs.getString("Editorial"));
+                lib.setEstado(rs.getBoolean("Estado"));
+
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe el Libro");
+
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Libros " + ex.getMessage());
+        }
+
+        return lib;
+    } 
 }
---
+++ src/main.java
@@ -0,0 +1,26 @@
+import Data.Conexion;
+import Data.LibroData;
+import Entidades.Libros;
+import java.sql.Connection;
+
+public class main {
+
+    Connection con = Conexion.getConexion();
+
+    public static void main(String[] args) {
+//        Libros L1 = new Libros("1234567891292", "La casa Blanca", "Rosa Sosa", 1973, "Politica", "Blum", true);
+//        Libros L2 = new Libros("1234567891235", "Biolagia1", "Jose Perez", 1990, "Humanidades", "Brisa", true);
+//        Libros L3 = new Libros("1234567891236", "El Test", "Jonathan Holms ", 2003, "Ficcion", "Proyecto", true);
+//        Libros L4 = new Libros("1234567891237", "El Descanso", "Mauro Scistri", 2000, "Novela", "Arbol", true);
+//        Libros L5 = new Libros("1234567891230", "Decada del 80", "Julio Lezcano", 1985, "Policial", "Guters", true);
+//        LibroData ld = new LibroData();
+       // ld.cargarLibro(L1);
+       // ld.cargarLibro(L2);
+       // ld.cargarLibro(L3);
+       // ld.cargarLibro(L4);
+       // ld.cargarLibro(L5);
+       // ld.cargarLibro(L5);
+//       ld.eliminarLibro(4);
+
+    }
+}
